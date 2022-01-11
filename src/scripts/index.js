f0d8228acfee60d4a0c6ac4db95f882213db446a
André Siqueira <andresiqueira-ti@outlook.com>
2022-01-10 22:37:11 -0300

    refatoração do javascript e começo da implementação do bootstrap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,88 +1,65 @@
-//import fetch from 'node-fetch'
-const name = document.querySelector('[data-js="name"]');
-const level = document.querySelector('[data-js="level"]');
-const vocation = document.querySelector('[data-js="vocation"]');
-const sex = document.querySelector('[data-js="sex"]');
-const status = document.querySelector('[data-js="status"]');
-const world = document.querySelector('[data-js="world"]');
-const residence = document.querySelector('[data-js="residence"]');
-const accountStatus = document.querySelector('[data-js="accont-status"]');
-const lastLogin = document.querySelector('[data-js="last-login"]');
 const erro = document.querySelector('[data-js="erro"]');
-
-
-
 const findCharacter = document.querySelector('[data-js="find-character"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
 
 function reqAPI (data) {
     fetch(`https://api.tibiadata.com/v2/characters/${data}.json`, {method: 'GET'})
     .then( response => {
-
-    return response.json();
+        return response.json();
     })
     .then(data => {
-
         if ( data.characters.error === "Character does not exist." ) {
             erro.textContent = 'Personagem não existe';
         }
 
+        const character = [
+            data.characters.data.name,
+            data.characters.data.sex,
+            data.characters.data.level,
+            data.characters.data.vocation,
+            data.characters.data.status,
+            data.characters.data.world,
+            data.characters.data.residence,
+            data.characters.data.account_status,
+            data.characters.data.last_login[0].date 
+        ]
 
-        const character = {
-            name: data.characters.data.name,
-            sex: data.characters.data.sex,
-            level: data.characters.data.level,
-            vocation: data.characters.data.vocation,
-            status: data.characters.data.status,
-            world: data.characters.data.world,
-            residence: data.characters.data.residence,
-            accountStatus: data.characters.data.account_status,
-            lastLogin: data.characters.data.last_login[0].date 
-        }
-
-        name.textContent = character.name;
-        sex.textContent = character.sex;
-        level.textContent = character.level;
-        vocation.textContent = character.vocation;
-        status.textContent = character.status;
-        world.textContent = character.world;
-        residence.textContent = character.residence;
-        accountStatus.textContent = character.accountStatus;
-        lastLogin.textContent = character.lastLogin;
-
-        findCharacter.value = '';
+        handleTable(character)
     })
 }
 
-
-
 submitButton.addEventListener('click', event => {
-    clearValues();
     event.preventDefault();
     reqAPI(validateField(findCharacter.value));
+    removeTable();
 })
 
-
-function clearValues () {
-    name.textContent = '';
-    sex.textContent = '';
-    level.textContent = '';
-    vocation.textContent = '';
-    status.textContent = '';
-    world.textContent = '';
-    residence.textContent = '';
-    accountStatus.textContent = '';
-    lastLogin.textContent = '';
-    erro.textContent = '';
+function removeTable () {
+    const table = document.querySelector('table');
+    table ? table.remove() : null
 }
-
 
 function validateField (field) {
     if (field) {
         return field;
     }
-    erro.textContent = 'Preencha o campo acima';
+    erro.textContent = 'Preencha o campo acima'
+    setTimeout(()=> { erro.textContent = ''}, 2000);
     return '';
 }
 
+function handleTable(...arguments) {
+    arguments.map( element =>{
+        const table = document.createElement('table');
+        element.forEach(elements => {
+            const column = document.createElement('tr');
+            const row = document.createElement('td');
+            row.textContent = elements;
+            column.appendChild(row);
+            table.appendChild(column);  
+            table.classList.add('table');
+            document.body.appendChild(table);
+        });
+    });   
+}
 
