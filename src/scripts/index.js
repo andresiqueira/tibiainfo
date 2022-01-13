6b1ab158048ff25d99a2f330e94aa4079fb6b6db
André Siqueira <andresiqueira-ti@outlook.com>
2022-01-12 23:44:01 -0300

    agora ta cheio de bug, amanha eu vejo :*

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,65 +1,85 @@
-const erro = document.querySelector('[data-js="erro"]');
-const findCharacter = document.querySelector('[data-js="find-character"]');
-const submitButton = document.querySelector('[data-js="submit-button"]');
+(function () {
+    const erro = document.querySelector('[data-js="erro"]');
+    const findCharacter = document.querySelector('[data-js="find-character"]');
+    const submitButton = document.querySelector('[data-js="submit-button"]');
 
-function reqAPI (data) {
-    fetch(`https://api.tibiadata.com/v2/characters/${data}.json`, {method: 'GET'})
-    .then( response => {
-        return response.json();
-    })
-    .then(data => {
-        if ( data.characters.error === "Character does not exist." ) {
-            erro.textContent = 'Personagem não existe';
-        }
+    function reqAPI (data) {
+        fetch(`https://api.tibiadata.com/v2/characters/${data}.json`, {method: 'GET'})
+        .then( response => {
+            return response.json();
+        })
+        .then(data => {
+            if ( data.characters.error === "Character does not exist." ) {
+                erro.textContent = 'Personagem não existe';
+            }
 
-        const character = [
-            data.characters.data.name,
-            data.characters.data.sex,
-            data.characters.data.level,
-            data.characters.data.vocation,
-            data.characters.data.status,
-            data.characters.data.world,
-            data.characters.data.residence,
-            data.characters.data.account_status,
-            data.characters.data.last_login[0].date 
-        ]
+            const character = Object.entries(data.characters.data);
+            const characterInfos = [
+                character[0],
+                character[2],
+                character[3],
+                character[4],
+                character[6],
+                character[7],
+                character[9],
+                character[10],
+                formatDate(character[8][1][0].date) 
+            ]
 
-        handleTable(character)
-    })
-}
-
-submitButton.addEventListener('click', event => {
-    event.preventDefault();
-    reqAPI(validateField(findCharacter.value));
-    removeTable();
-})
-
-function removeTable () {
-    const table = document.querySelector('table');
-    table ? table.remove() : null
-}
-
-function validateField (field) {
-    if (field) {
-        return field;
+            createTable(characterInfos)
+        })
     }
-    erro.textContent = 'Preencha o campo acima'
-    setTimeout(()=> { erro.textContent = ''}, 2000);
-    return '';
-}
 
-function handleTable(...arguments) {
-    arguments.map( element =>{
-        const table = document.createElement('table');
-        element.forEach(elements => {
-            const column = document.createElement('tr');
-            const row = document.createElement('td');
-            row.textContent = elements;
-            column.appendChild(row);
-            table.appendChild(column);  
-            table.classList.add('table');
-            document.body.appendChild(table);
-        });
-    });   
-}
+    submitButton.addEventListener('click', event => {
+        event.preventDefault();
+        reqAPI(validateInput(findCharacter.value));
+        removeTable();
+    });
 
+    function replaceComma(string) {
+        return string.replace(',',': ');
+    }
+
+    function acceptOnlyWordsAndWhiteSpace(inputValue) {
+        const regex = /[\d]|[^a-zA-Z ]/;
+        return regex.test(inputValue);
+    }
+
+    function validateInput (inputValue) {
+        if (inputValue && acceptOnlyWordsAndWhiteSpace(inputValue) === false) {
+            return inputValue;
+        }
+        erro.textContent = 'Preencha o campo com caracteres válidos'
+        setTimeout(()=> { erro.textContent = ''}, 2000);
+        return '';
+    }
+
+    function createTable(...argument) {
+        argument.map( elements =>{
+            const table = document.createElement('table');
+            elements.forEach(element => {
+                const column = document.createElement('tr');
+                const row = document.createElement('td');
+                row.textContent = replaceComma(element.toString())[0]
+                .toUpperCase() + replaceComma(element.toString())
+                .substring(1);
+                column.appendChild(row);
+                table.appendChild(column);  
+                table.classList.add('table');
+                document.body.appendChild(table);
+            });
+        });   
+    }
+
+    function removeTable () {
+        const table = document.querySelector('table');
+        table ? table.remove() : null
+    }
+
+    function formatDate(date) {
+        const dateToFormat = new Date(date);
+        const formattedDate = dateToFormat.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+        const formattedTime = dateToFormat.toLocaleTimeString('pt-BR')
+        return `${formattedDate}  ${formattedTime}`;
+    }
+})();
